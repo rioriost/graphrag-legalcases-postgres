cd794e125b308f599f49fdaaf97227239406e561
Serdar Mumcu <serdarmumcu@microsoft.com>
2024-11-18 21:12:54 +0300

    Remove Azure ML endpoint and key from main.bicep

--- scripts/setup_postgres_database.sh
+++ scripts/setup_postgres_database.sh
@@ -5,9 +5,7 @@ if [ $? -ne 0 ]; then
 fi
 POSTGRES_USERNAME=$(azd env get-value POSTGRES_USERNAME)
 POSTGRES_DATABASE=$(azd env get-value POSTGRES_DATABASE)
-AZURE_ML_SCORING_ENDPOINT=$(azd env get-value AZURE_ML_SCORING_ENDPOINT)
-AZURE_ML_ENDPOINT_KEY=$(azd env get-value AZURE_ML_ENDPOINT_KEY)
 
 . ./scripts/load_python_env.sh
 
-.venv/bin/python ./src/backend/fastapi_app/setup_postgres_legal_database.py --host $POSTGRES_HOST --username $POSTGRES_USERNAME --database $POSTGRES_DATABASE --mlendpoint $AZURE_ML_SCORING_ENDPOINT --mlkey $AZURE_ML_ENDPOINT_KEY
+.venv/bin/python ./src/backend/fastapi_app/setup_postgres_legal_database.py --host $POSTGRES_HOST --username $POSTGRES_USERNAME --database $POSTGRES_DATABASE
