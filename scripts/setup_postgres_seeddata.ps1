82dc46d653d7951b94455ba0e68536ec1ae5ba59
Serdar Mumcu <serdarmumcu@microsoft.com>
2025-01-03 16:29:36 +0300

    Fix Windows postprovisioning script

--- scripts/setup_postgres_seeddata.ps1
+++ scripts/setup_postgres_seeddata.ps1
@@ -5,10 +5,12 @@ if (-not $?) {
 }
 $POSTGRES_USERNAME = (azd env get-value POSTGRES_USERNAME)
 $POSTGRES_DATABASE = (azd env get-value POSTGRES_DATABASE)
+$APP_IDENTITY_NAME = (azd env get-value SERVICE_WEB_IDENTITY_NAME)
 
 if ([string]::IsNullOrEmpty($POSTGRES_HOST) -or [string]::IsNullOrEmpty($POSTGRES_USERNAME) -or [string]::IsNullOrEmpty($POSTGRES_DATABASE)) {
     Write-Host "Can't find POSTGRES_HOST, POSTGRES_USERNAME, and POSTGRES_DATABASE environment variables. Make sure you run azd up first."
     exit 1
 }
 
-python ./src/backend/fastapi_app/setup_postgres_legal_seeddata.py --host $POSTGRES_HOST --username $POSTGRES_USERNAME --database $POSTGRES_DATABASE
+python ./src/backend/fastapi_app/setup_postgres_legal_seeddata.py --host $POSTGRES_HOST --username $POSTGRES_USERNAME --database $POSTGRES_DATABASE  --app-identity-name $APP_IDENTITY_NAME
+python ./src/backend/fastapi_app/setup_postgres_legal_seeddata_msrgraphrag.py --host $POSTGRES_HOST --username $POSTGRES_USERNAME --database $POSTGRES_DATABASE
